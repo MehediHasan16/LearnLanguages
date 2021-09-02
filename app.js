868f4de359457bd3811641f791cf3b99ef47e102
MehediHasan16 <maher168082@gmail.com>
2021-09-02 19:49:22 +0600

    added js

--- app.js
+++ app.js
@@ -1,1 +1,49 @@
-console.log('book open')
+const searchResult = () => {
+    const inputField = document.getElementById("input-field");
+    const inputFieldText = inputField.value;
+
+
+
+
+
+    inputField.value = '';
+    const url = `https://openlibrary.org/search.json?q=${inputFieldText}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayLoad(data.docs));
+
+}
+const displayLoad = datas => {
+
+    console.log(datas)
+    // search resultNumber
+    const resultNumber = document.getElementById("result-number");
+    resultNumber.innerHTML = ` <h2>search result: ${datas.length}</h2>`
+
+    // search result details
+    const resultDetails = document.getElementById("result-details");
+    resultDetails.textContent = '';
+    datas.forEach(data => {
+        console.log(data)
+        const div = document.createElement('div')
+
+        div.classList.add('col')
+        div.innerHTML = `
+        <div id="inner-shadow" class="card h-100 shadow-lg">
+        <img src=" https://covers.openlibrary.org/b/id/${data.cover_i}-M.jpg" class="card-img-top  p-5" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${data.title}</h5>
+            <p class="card-text">Author Name: ${data.author_name[0]}<p>
+            <p class="card-text">Publisher : ${data.publisher[0]}<p>
+            <p class="card-text">Publish_Year : ${data.publish_year[0]}<p>
+        </div>
+    </div>
+        `;
+
+        resultDetails.appendChild(div);
+
+    });
+
+
+
+};
